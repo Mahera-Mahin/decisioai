14571ce1a619c754f0582721b53eaa125fb3765f
Mahera-Mahin <maheramahins@gmail.com>
2026-02-15 20:07:27 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "https://decisioai.onrender.com"
 
 st.set_page_config(page_title="DecisioAI", layout="wide")
 
